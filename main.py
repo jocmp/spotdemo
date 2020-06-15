8ef84794555706986bc057e08ae67e8ad1298979
Josiah Campbell <jocmp64@gmail.com>
2020-06-14 23:13:14 -0500

    Add form field on index

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
+def track_item(result):
+    (idx, track) = result
+    return {"index": idx + 1, "track_name": track['name']}
+
+
 def fetch_music(query):
+    if not query:
+        return []
+
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
     results = sp.search(q=query, limit=20)
-    serialized = []
-    for idx, track in enumerate(results['tracks']['items']):
-        serialized.append({"index": idx + 1, "track_name": track['name']})
-    
-    return serialized
+    return list(map(track_item, enumerate(results['tracks']['items'])))
 
 
 @app.route('/')
 def index():
     query = request.args.get('q', '')
     results = fetch_music(query=query)
-    return render_template('index.html', results=results)
+
+    return render_template('index.html', results=results, query=query)
